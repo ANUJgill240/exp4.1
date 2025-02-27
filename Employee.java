f617d582672457c962f4c20adeef036f6994de24
ANUJgill240 <anujgill240@gmail.com>
2025-02-27 14:15:20 +0530

    create Employee.java

---
+++ Employee.java
@@ -0,0 +1,20 @@
+package exp41;
+
+ import java.util.*;
+
+ class Employee {
+    int id;
+    String name;
+    double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Salary: " + salary;
+    }
+}
